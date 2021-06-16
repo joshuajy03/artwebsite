3217e69be9395300f94c8c99cf1722757a955e9a
joshuajy03 <52546898+joshuajy03@users.noreply.github.com>
2021-06-16 15:03:56 -0700

    Added Google Map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,6 +15,14 @@ function Contact() {
             PayPal: doublesheepcn@hotmail.com
           </p>
         </div>
+        <iframe
+          width="600"
+          height="450"
+          loading="lazy"
+          allowfullscreen
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCtb3f0glg8i6LcX-Rwj8BTMHOaiS920fU
+          &q=Newsom+Ave,+Cupertino,+CA+95014">
+        </iframe>
       </div>
     </div>
   );
